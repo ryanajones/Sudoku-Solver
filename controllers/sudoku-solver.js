61149f1072dfdbbf16ce1a96334455390aef87fc
Ryan Jones <ryanajonesdev@gmail.com>
2021-02-26 11:51:23 -0800

    added ability to solve sudoku puzzle using backtracking algorithm

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable class-methods-use-this */
 class SudokuSolver {
   validate(puzzleString) {}
@@ -8,7 +9,110 @@ class SudokuSolver {
 
   checkRegionPlacement(puzzleString, row, column, value) {}
 
-  solve(puzzleString) {}
+  solve(puzzleString) {
+    // Check to see if it is safe to place in current row, column, and box
+    const isSafe = (board, row, col, value) => {
+      // Check if number is unique in column
+      for (let i = 0; i < 9; i++) {
+        if (board[i][col] === value) {
+          return false;
+        }
+      }
+
+      // Check if number is unique in row
+      for (let j = 0; j < 9; j++) {
+        if (board[row][j] === value) {
+          return false;
+        }
+      }
+
+      // Check if number is unique in box
+      const boxTopRow = parseInt(row / 3) * 3; // Returns index of top row of box (0, 3, or 6)
+      const boxLeftColumn = parseInt(col / 3) * 3; // Returns index of left column of box (0, 3 or 6)
+      for (let k = boxTopRow; k < boxTopRow + 3; k++) {
+        for (let l = boxLeftColumn; l < boxLeftColumn + 3; l++) {
+          if (board[k][l] === value) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    };
+
+    const solveFromCell = (board, row, col) => {
+      // If on column 9 (outside row), move to next row and reset column to zero
+      if (col === 9) {
+        col = 0;
+        row++;
+      }
+
+      // If on row 9 (outside board), the solution is complete, so return the board
+      if (row === 9) {
+        return board;
+      }
+
+      // If already filled out (not empty) then skip to next column
+      if (board[row][col] !== '.') {
+        return solveFromCell(board, row, col + 1);
+      }
+
+      // Start with 1 and check if okay to place in cell. If so,
+      // run the algorithm from the next cell (col + 1), and see if
+      // false is not returned. A returned board indicates true, since
+      // a solution has been found. If false was returned, then empty out
+      // the cell, and try with next value
+      for (let i = 1; i < 10; i++) {
+        const valueToPlace = i.toString();
+        if (isSafe(board, row, col, valueToPlace)) {
+          board[row][col] = valueToPlace;
+          if (solveFromCell(board, row, col + 1) !== false) {
+            return solveFromCell(board, row, col + 1);
+          }
+          board[row][col] = '.';
+        }
+      }
+
+      // If solution not found yet, return false
+      return false;
+    };
+
+    // Turn puzzleString into array of rows and cols
+    const generateBoard = (values) => {
+      const board = [[], [], [], [], [], [], [], [], []];
+
+      let boardRow = -1;
+      for (let i = 0; i < values.length; i++) {
+        if (i % 9 === 0) {
+          boardRow += 1;
+        }
+        board[boardRow].push(values[i]);
+      }
+      return board;
+    };
+
+    const solveSudoku = () => {
+      const puzzleStringToArray = puzzleString.split('');
+
+      const originalBoard = generateBoard(puzzleStringToArray);
+
+      const solution = solveFromCell(originalBoard, 0, 0);
+
+      if (solution === false) return;
+
+      let i;
+      let j;
+      let solutionString = '';
+      for (i = 0; i < solution.length; i++) {
+        for (j = 0; j < solution[i].length; j++) {
+          solutionString += solution[i][j].toString();
+        }
+      }
+
+      return solutionString;
+    };
+    return solveSudoku();
+  }
 }
 
 module.exports = SudokuSolver;
--- routes/api.js
+++ routes/api.js
@@ -8,31 +8,35 @@ module.exports = function (app) {
     const { puzzle, coordinate, value } = req.body;
 
     // If puzzle, coordinate, or value is missing
-    if (!puzzle) {
-      return json.res({ error: 'Required fields missing' });
+    if (
+      puzzle === undefined ||
+      coordinate === undefined ||
+      value === undefined
+    ) {
+      return res.json({ error: 'Required field(s) missing' });
     }
 
     // If puzzle contains values that are not numbers or periods
-    const numberOrPeriodRegex = '[^.0-9]';
-    if (puzzleRegex.test(puzzle)) {
-      return json.res({ error: 'Invalid characters in puzzle' });
+    const numberOrPeriodRegex = /[^.0-9]/;
+    if (numberOrPeriodRegex.test(puzzle)) {
+      return res.json({ error: 'Invalid characters in puzzle' });
     }
 
     // If puzzle is greater or less than 81
     if (puzzle.length > 81 || puzzle.length < 81) {
-      return json.res({ error: 'Expected puzzle to be 81 characters long' });
+      return res.json({ error: 'Expected puzzle to be 81 characters long' });
     }
 
     // If coordinate doesn't point to existing square
     const rowsAndColumnRegex = /[A-I][1-9]/;
     if (!rowsAndColumnRegex.test(coordinate)) {
-      return json.res({ error: 'Invalid coordinate' });
+      return res.json({ error: 'Invalid coordinate' });
     }
 
     // If value is not a number between 1-9
     const numberRegex = /[1-9]/;
     if (!numberRegex.test(value)) {
-      return json.res({ error: 'Invalid value' });
+      return res.json({ error: 'Invalid value' });
     }
   });
 
@@ -40,21 +44,26 @@ module.exports = function (app) {
     const { puzzle } = req.body;
 
     // If puzzle is missing
-    if (!puzzle) {
-      return json.res({ error: 'Required fields missing' });
+    if (puzzle === undefined) {
+      return res.json({ error: 'Required field missing' });
     }
 
     // If puzzle contains values that are not numbers or periods
     const numberOrPeriodRegex = /[^.0-9]/g;
-    if (puzzleRegex.test(puzzle)) {
-      return json.res({ error: 'Invalid characters in puzzle' });
+    if (numberOrPeriodRegex.test(puzzle)) {
+      return res.json({ error: 'Invalid characters in puzzle' });
     }
 
     // If puzzle is greater or less than 81
     if (puzzle.length > 81 || puzzle.length < 81) {
-      return json.res({ error: 'Expected puzzle to be 81 characters long' });
+      return res.json({ error: 'Expected puzzle to be 81 characters long' });
     }
 
+    // Solve
+    const solutionString = solver.solve(puzzle);
+    if (solutionString) {
+      res.json({ solution: solutionString });
+    }
     // if puzzle is invalid or cannot be solved
   });
 };
